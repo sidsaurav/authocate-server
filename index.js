ef8f634811ac794f3c017c85c58dbae145208980
Siddharth Saurav <siddharth.saurav@perfios.com>
2023-07-27 00:36:30 +0530

    big progress with bugs (cant overwrite User model)

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const express = require('express')
 const morgan = require('morgan')
 const authRouter = require('./authRouter.js')
-const connectDB = require('./db.js')
+const mongoose = require('mongoose')
 
-const app = express()
-app.use(morgan('tiny'))
-app.use(express.json())
-connectDB()
-app.listen(5000, () => console.log('Server running on port 5000'))
+const initApp = (conn, JWT_SECRET_KEY) => {
+  const app = express()
+  app.use((req, res, next) => {
+    const userSchema = new mongoose.Schema(
+      {
+        username: {
+          type: String,
+          required: true,
+          unique: true,
+        },
+        password: {
+          type: String,
+          required: true,
+        },
+      },
+      { timestamps: true }
+    )
 
-//used for authentication
+    req.conn = conn
+    req.User = req.conn.model('User', userSchema)
+    next()
+  })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.use(authRouter)
+  app.listen(5000, () => console.log('Server running on port 5000'))
+  app.use(morgan('tiny'))
+  app.use(express.json())
 
-app.get('*', (req, res) => {
-  res.status(404).send('Page not found')
-})
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  app.use(authRouter(JWT_SECRET_KEY))
+
+  app.get('*', (req, res) => {
+    res.status(404).send('404 Error : Page not found :(')
+  })
+}
+module.exports = initApp
