6e963a430eb783523dc160cd722251076c1e5fa2
Siddharth Saurav <siddharth.saurav@perfios.com>
2023-07-27 17:14:37 +0530

    remove express dependency

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-const express = require('express')
-const morgan = require('morgan')
 const authRouter = require('./authRouter.js')
 const generateModel = require('./userSchema.js')
 
-const initApp = (conn, JWT_SECRET_KEY) => {
+const initApp = (app, conn, JWT_SECRET_KEY) => {
   console.log('Initializing authentication...')
-  const app = express()
 
   const User = generateModel(conn)
 
-  app.listen(5000, () => console.log('Server running on port 5000'))
   app.use((req, res, next) => {
     req.User = User
     next()
   })
-  app.use(morgan('tiny'))
-  app.use(express.json())
 
   app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' })
