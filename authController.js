abc917182beed401b410c815c7c92d3762b554f2
Siddharth Saurav <siddharth.saurav@perfios.com>
2023-07-27 15:41:52 +0530

    improve file structure (workings :))

--- authController.js
+++ authController.js
@@ -1,26 +1,6 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-//============================================================================
-const mongoose = require('mongoose')
-
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
-)
-
-//============================================================================
-
 const loginUser = (JWT_SECRET_KEY) => async (req, res) => {
   try {
     const username = req.body.username
