ebb6ed22465154ff3bd5c38fc1d415e68261534c
Siddharth Saurav <siddharth.saurav@perfios.com>
2023-07-28 15:18:16 +0530

    added authorise function

--- authRouter.js
+++ authRouter.js
@@ -9,7 +9,10 @@ const express = require('express')
 
 const authRouter = (JWT_SECRET_KEY) => {
   const router = express.Router()
-  router.route('/api/auth/login').get(getUser).post(loginUser(JWT_SECRET_KEY))
+  router
+    .route('/api/auth/login')
+    .get(authorise(JWT_SECRET_KEY), getUser)
+    .post(loginUser(JWT_SECRET_KEY))
   router.route('/api/auth/signup').post(signupUser(JWT_SECRET_KEY))
   router.route('/api/auth/test').get(getAllUsers)
   return router
